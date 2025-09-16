b83b187ad4bbeb92b69382f2d182686a3b88d612
Aditya Raj Singh <ranawataaditya06@gmail.com>
2025-09-16 15:36:02 +0530

    vite configuration.

--- vite.config.ts
+++ vite.config.ts
@@ -25,9 +25,9 @@ export default defineConfig({
   },
   root: path.resolve(import.meta.dirname, "client"),
   build: {
-    outDir: path.resolve(import.meta.dirname, "dist/public"),
-    emptyOutDir: true,
-  },
+  outDir: "dist", // relative to root (client folder)
+  emptyOutDir: true,
+},
   server: {
     fs: {
       strict: true,
