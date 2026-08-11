355e002eecb79699428635ad55be453ef4f9994b
shubham singh <32100002+Honeyzed@users.noreply.github.com>
2026-08-11 11:12:22 +0530

    add  more page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import Home from "@/pages/home";
 import Mentordetail from "./Mentordetail";
+import  MentorProfileDetail from "./MentorProfileDetail";
 import NotFound from "@/pages/not-found";
 
 function Router() {
@@ -12,6 +13,7 @@ function Router() {
     <Switch>
       <Route path="/" component={Home} />
       <Route path="/mentordetail" component={Mentordetail} />
+         <Route path="/mentorprofile" component={MentorProfileDetail} />
       <Route component={NotFound} />
     </Switch>
   );
